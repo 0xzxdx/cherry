73ddb6e7a9283c71427fa4e7c37be8b6888c41f1
0xzxdx <233373@gmail.com>
2024-09-06 15:20:27 +0800

    Added shacn UI.

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import "./tailwind.css";
+import styles from "./tailwind.css";
+import { LinksFunction } from "@remix-run/cloudflare";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
