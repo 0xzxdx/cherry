880019a10264757f5008613d7ed7c4210d074857
0xzxdx <233373@gmail.com>
2024-09-06 14:01:09 +0800

    first commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,19 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
