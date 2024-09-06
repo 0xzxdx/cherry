ce2a5512feadd339f3aaa8e64f7da65061d1d5d9
0xzxdx <233373@gmail.com>
2024-09-06 15:43:58 +0800

    Fix: Correct tailwind.css import method to resolve build error

--- app/root.tsx
+++ app/root.tsx
@@ -5,8 +5,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import styles from "./tailwind.css";
 import { LinksFunction } from "@remix-run/cloudflare";
+import styles from "./tailwind.css?url";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
